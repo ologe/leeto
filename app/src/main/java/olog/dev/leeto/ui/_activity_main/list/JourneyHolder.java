a927c79dc2b80f52a7663ce493d010fe2dc3acf9
Eugeniu Olog <eugeniu.olog@gmail.com>
2017-08-31 00:39:40 +0200

    android dagger

--- app/src/main/java/olog/dev/leeto/ui/_activity_main/list/JourneyHolder.java
+++ app/src/main/java/olog/dev/leeto/ui/_activity_main/list/JourneyHolder.java
@@ -1,15 +1,23 @@
 package olog.dev.leeto.ui._activity_main.list;
 
+import android.support.annotation.DrawableRes;
+import android.support.constraint.ConstraintLayout;
+import android.support.constraint.ConstraintSet;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import olog.dev.leeto.R;
 import olog.dev.leeto.data.model.Journey;
+import olog.dev.leeto.data.model.Location;
 import olog.dev.leeto.data.model.Stop;
 import olog.dev.leeto.ui.activity_detail.DetailActivity;
+import olog.dev.leeto.utility.AppConstants;
 import olog.dev.leeto.utility.DateUtils;
 
 public class JourneyHolder extends RecyclerView.ViewHolder {
@@ -19,13 +27,23 @@ public class JourneyHolder extends RecyclerView.ViewHolder {
     @BindView(R.id.journeyDate) TextView journeyDate;
     @BindView(R.id.location) TextView location;
     @BindView(R.id.journeyDescription) TextView journeyDescription;
+    @BindView(R.id.img) ImageView image;
+
+    private ConstraintLayout constraintLayout;
+    private ConstraintSet constraintSet;
 
     JourneyHolder(View view) {
         super(view);
         ButterKnife.bind(this, view);
+
+        constraintLayout = ((ConstraintLayout) itemView);
+        constraintSet = new ConstraintSet();
+        constraintSet.clone(constraintLayout);
     }
 
     void bind(Journey journey, int position){
+        if(position == RecyclerView.NO_POSITION) return;
+
         Stop firstStop = journey.getFirstStop();
         String date = DateUtils.toString(firstStop.getDate());
 
@@ -36,6 +54,26 @@ public class JourneyHolder extends RecyclerView.ViewHolder {
 
         scrim.setTransitionName(DetailActivity.SHARED_ROOT + position);
         journeyName.setTransitionName(DetailActivity.SHARED_JOURNEY_NAME + position);
+
+        constraintSet.setDimensionRatio(image.getId(), AppConstants.getRatio(position));
+        constraintSet.applyTo(constraintLayout);
+
+        Glide.with(itemView.getContext())
+                .load(getCityImage(firstStop.getLocation()))
+                .centerCrop()
+                .error(R.color.dark_grey)
+                .into(image);
+    }
+
+    @DrawableRes
+    private int getCityImage(Location location){
+        String city = location.getName();
+        if(city.equals("Mosca")) return R.drawable.mosca;
+        if(city.equals("Parma")) return R.drawable.parma;
+        if(city.equals("Parigi")) return R.drawable.parigi;
+        if(city.equals("New York")) return R.drawable.new_york;
+        if(city.equals("Barcellona")) return R.drawable.barcellona;
+        return -1;
     }
 
 }
--- app/src/main/java/olog/dev/leeto/utility/AppConstants.java
+++ app/src/main/java/olog/dev/leeto/utility/AppConstants.java
@@ -3,6 +3,9 @@ package olog.dev.leeto.utility;
 import android.content.Context;
 import android.support.annotation.NonNull;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import lombok.experimental.UtilityClass;
 import olog.dev.leeto.R;
 
@@ -10,9 +13,24 @@ import olog.dev.leeto.R;
 public class AppConstants {
 
     public static String NO_DESCRIPTION = "";
+    private static List<String> ratioList;
+
+    private static final String RATIO_16_9 = "1.77";
+    private static final String RATIO_2_1 = "2";
+    private static final String RATIO_SQUARE = "1";
+    private static final String RATIO_ONE_HALF = "1.5";
 
     public static void init(@NonNull Context context){
         NO_DESCRIPTION = context.getString(R.string.no_description);
+        ratioList = new ArrayList<>();
+        ratioList.add(RATIO_16_9);
+        ratioList.add(RATIO_2_1);
+        ratioList.add(RATIO_SQUARE);
+        ratioList.add(RATIO_ONE_HALF);
+    }
+
+    public String getRatio(int position){
+        return ratioList.get(position % ratioList.size());
     }
 
 }
